435cb5e29a1b0afa5b9b0948b0f3e20a9824d1c3
Carolina de Abreu Fernandes <carolinaabreu.f@gmail.com>
2020-11-12 00:46:18 -0300

    Modified - 12/11/2020

---
+++ views/Page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function Page() {
+  return (
+    <View>
+      <Text>DEC - Diagnóstico de Energia do Consumidor</Text>
+    </View>
+  );
+}
