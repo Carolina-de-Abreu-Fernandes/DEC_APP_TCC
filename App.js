02976514d2a0911565f37224194b4f8b334b5fab
Carolina de Abreu Fernandes <carolinaabreu.f@gmail.com>
2020-11-19 10:33:38 -0300

    New Att - 19/11/2020

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './views/PageLogin';
+import NameForm from './views/PageLogin';
+import Input from './views/InputForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Login />
       <StatusBar style="auto" />
+      <NameForm />
+      <Input />
     </View>
   );
 }
